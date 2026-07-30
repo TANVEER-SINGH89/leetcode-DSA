35b356d93604e8d98e7e6e1116a984764d42f860
Tanveer Singh <tanveersikh@gmail.com>
2026-07-30 20:34:40 +0530

    Solution3

---
+++ Linked  list/Solution3.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    int rows;
+    int cols;
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+
+        rows = image.length;
+        cols = image[0].length;
+
+        int originalColor = image[sr][sc];
+
+        if(originalColor == color) {
+            return image;
+        }
+
+        dfs(image, sr, sc, originalColor, color);
+
+        return image;
+    }
+
+
+    private void dfs(int[][] image, int r, int c, int originalColor, int color) {
+
+        if(r < 0 || c < 0 || r >= rows || c >= cols 
+           || image[r][c] != originalColor) {
+            return;
+        }
+
+        image[r][c] = color;
+
+        dfs(image, r+1, c, originalColor, color);
+        dfs(image, r-1, c, originalColor, color);
+        dfs(image, r, c+1, originalColor, color);
+        dfs(image, r, c-1, originalColor, color);
+    }
+}
